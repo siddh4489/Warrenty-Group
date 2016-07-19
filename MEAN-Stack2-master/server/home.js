9355d5a1a640233266b4f6aa07db426bb334f76d
dev-eternus <siddhraj.atodaria@eternussolutions.com>
2016-07-19 17:45:37 +0530

    Create home.js

---
+++ MEAN-Stack2-master/server/home.js
@@ -0,0 +1,46 @@
+var db = require('./pghelper'),
+    config = require('./config'),
+    nforce = require('nforce'),
+     
+
+    userName = config.api.userName,
+    password = config.api.password;
+
+    org = nforce.createConnection({
+        clientId: config.api.clientId,
+        clientSecret: config.api.clientSecret,
+        redirectUri: config.api.redirectUri,
+        apiVersion: config.api.apiVersion,  // optional, defaults to current salesforce API version
+        environment: 'production',  // optional, salesforce 'sandbox' or 'production', production default
+        mode: 'single' // optional, 'single' or 'multi' user mode, multi default
+    });
+
+
+org.authenticate({ username: userName, password: password}, function(err, resp) {
+    if(!err) {
+        console.log('nforce connection succeeded');
+     
+    } else {
+        console.log('nforce connection failed: ' + err.message);
+     
+    }
+});
+
+function createHomes(req, res, next) {
+
+    
+};
+
+function revokeToken(req, res, next) {
+    org.revokeToken({token: org.oauth.access_token}, function(err) {
+        if (err) {
+            return next(err);
+        } else {
+            res.send('ok');
+        }
+    });
+
+}
+
+exports.createHomes = createHomes;
+exports.revokeToken = revokeToken;
