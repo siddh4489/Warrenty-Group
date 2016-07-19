7cb9a63cdecf7f061d1ee36d27d0c10dbcd56765
dev-eternus <siddhraj.atodaria@eternussolutions.com>
2016-07-19 16:16:17 +0530

    Update claim.js

--- MEAN-Stack2-master/client/js/claim.js
+++ MEAN-Stack2-master/client/js/claim.js
@@ -1,78 +1,40 @@
-angular.module('nibs.case', ['nibs.config'])
+angular.module('nibs.claim', ['nibs.config'])
 
     // Routes
     .config(function ($stateProvider) {
         $stateProvider
             .state('app.claim', {
-                url: "/help",
+                url: "/claimform",
                 views: {
                     'menuContent' :{
                         templateUrl: "templates/claim.html",
-                        controller: "CaseCtrl"
+                        controller: "ClaimController"
                     }
                 }
             })
     })
 
     // Services
-    .factory('Case', function ($http, $rootScope) {
+    .factory('Claim', function ($http, $rootScope) {
         return {
-            create: function(theCase) {
-                return $http.post($rootScope.server.url + '/cases/', theCase);
+            create: function(theClaim) {
+                return $http.post($rootScope.server.url + '/claims/', theClaim);
             }
         };
     })
 
     //Controllers
-    .controller('CaseCtrl', function ($scope, $window, $ionicPopup, Case, User) {
-        var user = JSON.parse($window.localStorage.getItem('user'));
-        console.log('the user email is'+user.email);
-        startChat();
-        var SOSPlugin = {
-            createEvent: function(Email) {
-            //alert('sos method called');
-                cordova.exec(
-                    null,
-                    null,
-                    'SOSPlugin', // mapped to our native Java class called "CalendarPlugin"
-                    'callNativeMethod', // with this action name
-                    [{                  // and this array of custom arguments to create our entry
-                        "Email": user.email,
-                    }]
-                );
-             }
-             }
+    .controller('ClaimController', function ($scope, $window, $ionicPopup, Claims, User) {
+       
 
-        $scope.case = {};
+        $scope.claim = {};
 
         $scope.submit = function () {
-            if($scope.case.subject=='undefined' || $scope.case.description=='undefined' || $scope.case.subject=='' || $scope.case.description==''){
-                $ionicPopup.alert({title: 'Alert', content: 'Please enter the Subject/Description.'});
-            }else{
-                Case.create($scope.case).success(function() {
+            
+                Claim.create($scope.claim).success(function() {
                      $ionicPopup.alert({title: 'Thank You', content: 'A customer representative will contact you shortly.'});
                 });
-            }
+          
         };
 
-        function startChat(){
-            //alert('startchat called');
-            if (!window._laq) { window._laq = []; }
-                window._laq.push(function(){liveagent.showWhenOnline('573j0000000GzbP', document.getElementById('liveagent_button_online_573j0000000GzbP'));
-               // liveagent.showWhenOffline('573j0000000GzbP', document.getElementById('liveagent_button_offline_573j0000000GzbP'));
-            });
-             liveagent.init('https://d.la2w2.salesforceliveagent.com/chat', '572j0000000Gx9L', '00Dj0000001quZb');
-        }
-
-        $scope.chat = function() {
-            var user = JSON.parse($window.localStorage.getItem('user'));
-            //alert('Live chat called');
-            startChat();
-        };
-
-//        $scope.sos = function(){
-//           //alert('sos called')
-//           $window.location = 'sos://' + user.email;
-//           SOSPlugin.createEvent(user.email);
-//        };
     });
