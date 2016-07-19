c9d2971dbe9c0989b2503987db7a9fa4b0fb03ba
dev-eternus <siddhraj.atodaria@eternussolutions.com>
2016-07-19 15:14:30 +0530

    Update claim.js

--- MEAN-Stack2-master/client/js/claim.js
+++ MEAN-Stack2-master/client/js/claim.js
@@ -30,28 +30,13 @@ angular.module('nibs.claim', ['nibs.config'])
         $scope.claim = {};
 
         $scope.submit = function () {
-            if($scope.case.subject=='undefined' || $scope.case.description=='undefined' || $scope.case.subject=='' || $scope.case.description==''){
+            if($scope.claim.subject=='undefined' || $scope.claim.description=='undefined' || $scope.claim.subject=='' || $scope.claim.description==''){
                 $ionicPopup.alert({title: 'Alert', content: 'Please enter the Subject/Description.'});
             }else{
-                Case.create($scope.case).success(function() {
+                Claim.create($scope.claim).success(function() {
                      $ionicPopup.alert({title: 'Thank You', content: 'A customer representative will contact you shortly.'});
                 });
             }
-        };
-
-        function startChat(){
-            //alert('startchat called');
-            if (!window._laq) { window._laq = []; }
-                window._laq.push(function(){liveagent.showWhenOnline('573j0000000GzbP', document.getElementById('liveagent_button_online_573j0000000GzbP'));
-               // liveagent.showWhenOffline('573j0000000GzbP', document.getElementById('liveagent_button_offline_573j0000000GzbP'));
-            });
-             liveagent.init('https://d.la2w2.salesforceliveagent.com/chat', '572j0000000Gx9L', '00Dj0000001quZb');
-        }
-
-        $scope.chat = function() {
-            var user = JSON.parse($window.localStorage.getItem('user'));
-            //alert('Live chat called');
-            startChat();
         };
 
 
