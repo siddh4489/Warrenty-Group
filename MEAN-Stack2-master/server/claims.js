5d58b8907462e93d79dc144bd2766a80f2c289fc
dev-eternus <siddhraj.atodaria@eternussolutions.com>
2016-07-19 16:21:02 +0530

    Update claims.js

--- MEAN-Stack2-master/server/claims.js
+++ MEAN-Stack2-master/server/claims.js
@@ -1,4 +1,4 @@
-var db = require('./pghelper'),
+    var db = require('./pghelper'),
     config = require('./config'),
     nforce = require('nforce'),
      
@@ -28,44 +28,7 @@ org.authenticate({ username: userName, password: password}, function(err, resp) 
 
 function createClaims(req, res, next) {
 
-    db.query('SELECT sfid FROM salesforce.contact WHERE id=$1',[req.userId], true)
-        .then(function (user) {
-            console.log("sfid: " + user.sfid);
-            // case is a reserved word. using _case instead.
-            var _case = nforce.createSObject('Case');
-            _case.set('contactId', user.sfid);
-            _case.set('subject', req.body.subject);
-            _case.set('description', req.body.description);
-            _case.set('origin', 'Web');
-            _case.set('status', 'New');
-
-            org.insert({ sobject: _case}, function(err, resp){
-                if (err) {
-                    console.log('First case insert failed: ' + JSON.stringify(err));
-                    org.authenticate({username: userName, password: password}, function(err) {
-                        if (err) {
-                            console.log('Authentication failed: ' + JSON.stringify(err));
-                            return next(err);
-                        } else {
-                            // retry
-                            org.insert({ sobject: _case}, function(err, resp) {
-                                if (err) {
-                                    console.log('Second case insert failed: ' + JSON.stringify(err));
-                                    return next(err);
-                                } else {
-                                    console.log('Second case insert worked');
-                                    return res.send('ok');
-                                }
-                            });
-                        }
-                    })
-                } else {
-                    console.log('First case insert worked');
-                    res.send('ok');
-                }
-            });
-        })
-        .catch(next);
+    
 };
 
 function revokeToken(req, res, next) {
